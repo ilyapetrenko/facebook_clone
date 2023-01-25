22e469426f92f6c058389417265e0ddfa4de2668
ilyapetrenko <91661624+ilyapetrenko@users.noreply.github.com>
2023-01-25 05:36:27 +0200

    With reselect

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -1,6 +1,17 @@
-export const getUsers = (state)  => {
+import { createSelector } from 'reselect'
+//селекторы зависят от маленьких частей стейта
+
+const getUsersSelector = (state)  => {
     return state.usersPage.users
 }
+//первый параметр это то что нужно получить как значение что бы закинуть в юзерс
+//из примитивных селекторов создаем навороченные
+//по порядку засовывается каждый примитивный селектор в функцию
+export const getUsers = createSelector(getUsersSelector, //getIsFetching,
+    (users, //isFetching
+    ) => {
+    return users.filter(u => true)
+})
 
 export const getPageSize = (state)  => {
     return state.usersPage.pageSize
