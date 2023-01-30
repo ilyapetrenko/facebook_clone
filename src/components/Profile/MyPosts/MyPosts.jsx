f5c36cca784d74a1794a38f36989933d6b598777
ilyapetrenko <91661624+ilyapetrenko@users.noreply.github.com>
2023-01-30 07:32:16 +0200

    Optimization with PureComponent

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,31 +1,35 @@
 import React from 'react'
 import classes from './MyPosts.module.css'
 import Post from './Post/Post'
-import { updateNewPostTextCreator, addPostCreator } from '../../../redux/profile-reducer'
 import AddPostForm from './AddPostForm'
 
-const MyPosts = (props) => {
-
-    let postsElements = props.posts.map(p => <Post message = {p.message} likesCount = {p.likesCount} />)
-
-    // let newPostElement = React.createRef()
-
-    // let onAddPost = () => {
-    //     props.addPost()
-    // }
-    //
-    // let onPostChange = () => {
-    //     let text = newPostElement.current.value
-    //     props.updateNewPostText(text)
+class MyPosts extends React.PureComponent {
+    // shouldComponentUpdate(nextProps, nextState, nextContext) {
+    //     return nextProps != this.props || nextState != this.state
     // }
 
-    return(
-        <div className={classes.postsBlock}>
-            <AddPostForm addPost={props.addPost}/>
-        <div className={classes.posts}>
-            {postsElements}
-        </div>
-    </div>)
+    render() {
+        let postsElements = this.props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
+
+        // let newPostElement = React.createRef()
+
+        // let onAddPost = () => {
+        //     props.addPost()
+        // }
+        //
+        // let onPostChange = () => {
+        //     let text = newPostElement.current.value
+        //     props.updateNewPostText(text)
+        // }
+
+        return (
+            <div className={classes.postsBlock}>
+                <AddPostForm addPost={this.props.addPost}/>
+                <div className={classes.posts}>
+                    {postsElements}
+                </div>
+            </div>)
+    }
 }
 
 export default MyPosts
