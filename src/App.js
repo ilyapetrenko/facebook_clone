9d9853fefdb2d8c857831f091521ad3c00b79c67
ilyapetrenko <91661624+ilyapetrenko@users.noreply.github.com>
2023-01-31 00:56:57 +0200

    lil fix

--- src/App.js
+++ src/App.js
@@ -7,12 +7,11 @@ import UsersContainer from './components/Users/UsersContainer'
 import ProfileContainer from './components/Profile/ProfileContainer'
 import HeaderContainer from './components/Header/HeaderContainer'
 import LoginPage from './components/Login/Login'
-import { connect, Provider } from 'react-redux'
+import { connect } from 'react-redux'
 import { compose } from 'redux'
 import withRouter from './components/Custom/withRouter'
 import { initializeApp } from './redux/app-reducer'
 import Preloader from './components/common/Preloader/Preloader'
-import store from './redux/redux-store'
 
 
 class App extends React.Component {
@@ -25,8 +24,6 @@ class App extends React.Component {
             return <Preloader/>
         }
         return (
-            <Provider store={store}>
-                <BrowserRouter>
                 <div className="app-wrapper">
                     <HeaderContainer/>
                     <Navbar/>
@@ -41,8 +38,6 @@ class App extends React.Component {
                         </Routes>
                     </div>
                 </div>
-                </BrowserRouter>
-            </Provider>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
+import store from './redux/redux-store'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
@@ -12,7 +15,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 // }, 1000)
 
 root.render(
-    <App/>
+    <Provider store={store}>
+        <BrowserRouter>
+        <App/>
+        </BrowserRouter>
+    </Provider>
 )
 
 reportWebVitals()
