b4047942647b2920a9d5c7b3c492bef8fffcf245
ilyapetrenko <91661624+ilyapetrenko@users.noreply.github.com>
2023-02-01 14:50:07 +0200

    +Redux DevTools

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, legacy_createStore as createStore } from 'redux'
+import { applyMiddleware, combineReducers, compose, legacy_createStore as createStore } from 'redux'
 import profileReducer from './profile-reducer'
 import dialogsReducer from './dialogs-reducer'
 import sidebarReducer from './sidebar-reducer'
@@ -16,6 +16,7 @@ let reducers = combineReducers({
     app: appReducer
 })
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware))
+const composeEnhancers = window.window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  || compose
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)))
 
 export default store
